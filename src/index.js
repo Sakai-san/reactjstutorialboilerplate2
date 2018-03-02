7b8da29a3ba1ab7c0fa57142eafffb217bbf0927
Sakai Toma <o_edo@hotmail.com>
2018-03-02 16:11:58 +0100

    Remove hashRouter.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import allReducers from './reducers';
 import './styles.css';
 import App from './components/App';
@@ -14,9 +14,9 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 );
